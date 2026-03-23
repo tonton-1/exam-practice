e80119bfaa93d19d1988d1071b0611c8e0b51356
ton <wattanachai.pu@ku.th>
2026-03-23 12:38:18 +0700

    Deploy Flutter web build1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -171,7 +171,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "62ec3b6df26e4106d86e974939d9f46e
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "001f826f851cc66c434d173c744e6498",
+"flutter_bootstrap.js": "aac5136dcebd4ebb10905c8c174ec634",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
